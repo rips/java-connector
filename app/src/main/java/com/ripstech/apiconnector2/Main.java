f64549b13cd4c0090736d5e9055330793347326c
Malena Ebert <mebert@ripstech.com>
2018-07-31 12:50:24 +0200

    clean up examples in main app

--- app/src/main/java/com/ripstech/apiconnector2/Main.java
+++ app/src/main/java/com/ripstech/apiconnector2/Main.java
@@ -3,6 +3,8 @@ package com.ripstech.apiconnector2;
 import com.ripstech.apiconnector2.config.HttpClientConfig;
 import com.ripstech.apiconnector2.connector.path.application.ScanWoIdPath;
 import com.ripstech.apiconnector2.connector.path.application.scan.IssueWoIdPath;
+import com.ripstech.apiconnector2.entity.receive.application.scan.Issue;
+import com.ripstech.apiconnector2.entity.receive.application.scan.issue.Type;
 import com.ripstech.apiconnector2.entity.send.LogSend;
 import com.ripstech.apiconnector2.entity.send.OrganisationSend;
 import com.ripstech.apiconnector2.entity.send.SettingSend;
@@ -13,36 +15,39 @@ import com.ripstech.apiconnector2.service.queryparameter.Filter;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 import java.time.OffsetDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.function.BiConsumer;
 
+import static com.ripstech.apiconnector2.service.queryparameter.JsonFilter.equal;
+import static com.ripstech.apiconnector2.service.queryparameter.JsonFilter.or;
+
 public class Main {
 
-	public static void main(String... args) throws IOException, ApiException, KeyManagementException, NoSuchAlgorithmException {
+	public static void main(String... args) throws IOException, ApiException {
 
 		InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("config.properties");
 		Properties properties = new Properties();
 		properties.load(inputStream);
 
 		Api api = new Api.Builder(properties.getProperty("api.baseuri"))
-				.withHttpClientConfig(new HttpClientConfig()
-						                      .setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 8181)))
-						                      .acceptAllCerts()
-						                      .setUserAgent("rpi api connector/2.0.0")
-				                     )
-				.withOAuthv2(properties.getProperty("api.username"), properties.getProperty("api.password"))
-				.build();
+				.withHttpClientConfig(new HttpClientConfig().setUserAgent("rpi api connector/2.0.0"))
+				          .withXPassword(properties.getProperty("api.username"), properties.getProperty("api.password"))
+				          .build();
 
-		//Configuration.strict = true;
-
-		System.out.println(api.getAccessToken());
+		api.application(2)
+				.scan(2)
+				.issues()
+				.get(new Filter().json(or(
+						equal("typeCategory", "client_vulnerability"),
+						equal("typeCategory", "server_vulnerability"))))
+				.process(issues -> issues.stream()
+						                   .map(Issue::getType)
+						                   .map(Type::getCategory)
+						                   .forEach(System.out::println),
+				         printError());
 
 		api.status().get().process(System.out::println, printError());
 
@@ -73,10 +78,16 @@ public class Main {
 				printError());
 
 		api.organisations().get().process(System.out::println, printError());
-		api.organisations().post(OrganisationSend.createPost("test orga").setTrialIssueTypes(Arrays.asList("123", "234"))).process(organisation -> {
-			System.out.println(organisation);
-			api.organisations().patch(organisation.getId(), OrganisationSend.createPatch().setValidUntil(OffsetDateTime.now().plusMonths(1)).setTrialIssueTypes(null)).process(System.out::println, printError());
-			api.organisations().delete(organisation.getId()).ifNotOk(printError());
+		api.organisations()
+				.post(OrganisationSend.createPost("test orga").setTrialIssueTypes(Arrays.asList("123", "234")))
+				.process(organisation -> {
+					System.out.println(organisation);
+					api.organisations().patch(organisation.getId(), OrganisationSend.createPatch()
+							                                                .setValidUntil(OffsetDateTime.now()
+									                                                               .plusMonths(1))
+							                                                .setTrialIssueTypes(null))
+							.process(System.out::println, printError());
+					api.organisations().delete(organisation.getId()).ifNotOk(printError());
 		}, printError());
 
 		ScanWoIdPath scanPath = api.applications().scans();
@@ -86,25 +97,24 @@ public class Main {
 		issuePath.reviewTypes().get().process(System.out::println, printError());
 		scanPath.stats().get(new Filter().limit(5)).process(System.out::println, printError());
 
-
-
 			api.applications().get().ifOk(apps -> apps.stream().findFirst().ifPresent(app -> {
 				api.application(app.getId()).scans().get().ifOk(scans -> scans.stream().findFirst().ifPresent(scan -> {
-					api.application(app.getId()).scans().patch(scan.getId(), ScanSend.createPatch().setTags(Arrays.asList("TAG1", "TAG2"))).ifOk(newScan -> {
-						System.out.println(newScan.getTags());
-					});
-					api.application(app.getId()).scans().patch(scan.getId(), ScanSend.createPatch().setTags(Collections.singletonList("TAG3"))).ifOk(newScan -> {
-						System.out.println(newScan.getTags());
-					});
-					api.application(app.getId()).scans().patch(scan.getId(), ScanSend.createPatch().setTags(null)).ifOk(newScan -> {
-						System.out.println(newScan.getTags());
-					});
-
-
-					api.application(app.getId()).scans().post(
-							ScanPost.createPost(ScanPost.ScanSub.createPost("123").setAnalysisDepth(5))
-					                                      ).process(System.out::print, printError());
-
+					api.application(app.getId())
+							.scans()
+							.patch(scan.getId(), ScanSend.createPatch().setTags(Arrays.asList("TAG1", "TAG2")))
+							.ifOk(newScan -> System.out.println(newScan.getTags()));
+					api.application(app.getId())
+							.scans()
+							.patch(scan.getId(), ScanSend.createPatch().setTags(Collections.singletonList("TAG3")))
+							.ifOk(newScan -> System.out.println(newScan.getTags()));
+					api.application(app.getId())
+							.scans()
+							.patch(scan.getId(), ScanSend.createPatch().setTags(null))
+							.ifOk(newScan -> System.out.println(newScan.getTags()));
+					api.application(app.getId())
+							.scans()
+							.post(ScanPost.createPost(ScanPost.ScanSub.createPost("123").setAnalysisDepth(5)))
+							.process(System.out::print, printError());
 				}));
 			}));
 
