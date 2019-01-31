646c190cc60d89a9c816f6d5e6631924d676d8d0
Amin Dada <adada@ripstech.com>
2019-01-31 16:44:25 +0100

    Fixed ACJ-151

--- utils/src/test/java/com/ripstech/api/utils/file/RipsFileFilterTest.java
+++ utils/src/test/java/com/ripstech/api/utils/file/RipsFileFilterTest.java
@@ -1,40 +1,42 @@
 package com.ripstech.api.utils.file;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import com.ripstech.api.utils.constant.RipsDefault;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 import static org.mockito.Mockito.spy;
 
-@Disabled
+
 public class RipsFileFilterTest {
-	private RipsFileFilter ripsFileFilter;
-	private List<File> files = Arrays.asList(new File("test.java"),
+	private RipsFileFilter ripsFileFilter = new RipsFileFilter(new HashSet<>(Arrays.asList("java", "jar")));
+	private static List<File> files = Arrays.asList(new File("test.java"),
 	                                         new File("test.jar"),
 	                                         new File("test.class"),
 	                                         new File("test.php"));
-
-
-
-	@BeforeEach
-	public void setUp() {
-		ripsFileFilter = spy(RipsFileFilter.class);
-	}
-
-	@Test
-	public void should_fail_filter() {
-		Assertions.assertThrows(IllegalArgumentException.class, () -> ripsFileFilter.filter(null));
+	@BeforeAll
+	static void createFiles() throws IOException {
+		for (File file : files) {
+			file.createNewFile();
+		}
 	}
 
 	@Test
 	public void should_pass_filter() {
+
 		List<File> filteredFiles = ripsFileFilter.filter(files);
-		Assertions.assertEquals(filteredFiles.size(), 2);
+		Assertions.assertEquals(2, filteredFiles.size());
+	}
+
+	@AfterAll
+	static void deleteFiles() {
+		for (File file : files) {
+			file.delete();
+		}
 	}
 
 }
