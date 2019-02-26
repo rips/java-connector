cf17b8b87a85a8e156928c623d721ec682dca6ae
Malena Ebert <mebert@ripstech.com>
2019-02-26 12:05:44 +0100

    Exposes versions of IncompatibleApiVersionException.

--- utils/src/main/java/com/ripstech/api/utils/exception/IncompatibleApiVersionException.java
+++ utils/src/main/java/com/ripstech/api/utils/exception/IncompatibleApiVersionException.java
@@ -5,8 +5,20 @@ import com.ripstech.api.utils.validation.ApiVersion;
 
 public class IncompatibleApiVersionException extends ApiException {
 
+	private final ApiVersion required;
+	private final ApiVersion found;
+
 	public IncompatibleApiVersionException(ApiVersion required, ApiVersion found) {
 		super(String.format("Incompatible RIPS API version (required: %s, found: %s)", required, found));
+		this.required = required;
+		this.found = found;
 	}
 
+	public ApiVersion getRequired() {
+		return required;
+	}
+
+	public ApiVersion getFound() {
+		return found;
+	}
 }
