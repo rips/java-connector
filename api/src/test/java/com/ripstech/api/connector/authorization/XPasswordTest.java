f3eb8aab6c5242eacb616b65ffec19110271b338
Malena Ebert <mebert@ripstech.com>
2019-03-22 14:57:16 +0100

    Implement encoded auth headers for XPassword mechanism, if the password or email contains special chars.

--- api/src/test/java/com/ripstech/api/connector/authorization/XPasswordTest.java
+++ api/src/test/java/com/ripstech/api/connector/authorization/XPasswordTest.java
@@ -1,31 +1,37 @@
 package com.ripstech.api.connector.authorization;
 
-import com.ripstech.api.connector.Api;
-import com.ripstech.api.connector.exception.ApiException;
-import com.ripstech.api.connector.service.ApiSettings;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-class XPasswordTest extends ApiSettings {
-
-	public XPasswordTest() throws IOException, ApiException {
-	}
+class XPasswordTest {
 
 	@Test
-	void header() {
+	void getAuthHeader() {
 		Map<String, String> authHeader = new XPassword("user", "pass").getAuthHeader();
 		assertEquals("user", authHeader.get("X-Api-Email"));
 		assertEquals("pass", authHeader.get("X-Api-Password"));
+		assertEquals(base64Encode("user"), authHeader.get("X-Api-Email-Enc"));
+		assertEquals(base64Encode("pass"), authHeader.get("X-Api-Password-Enc"));
 	}
 
 	@Test
-	void connect() throws ApiException {
-		Api api = new Api.Builder(BASE_URL).withXPassword(USERNAME, PASSWORD).build();
-		assertEquals(USERNAME, api.status().get().orThrow(ApiException::new).getUser().getEmail());
+	void getAuthHeaderWithSpecialChars() {
+		final String pass = "ÉhNí-:G®¥SÍ@ûfÐ²7b8Ô¢23¨xQ»ì¨²6{";
+		Map<String, String> authHeader = new XPassword("user", pass).getAuthHeader();
+		assertNull(authHeader.get("X-Api-Email"));
+		assertNull(authHeader.get("X-Api-Password"));
+		assertEquals(base64Encode("user"), authHeader.get("X-Api-Email-Enc"));
+		assertEquals(base64Encode(pass), authHeader.get("X-Api-Password-Enc"));
+	}
+
+	private String base64Encode(String s) {
+		return Base64.getEncoder().encodeToString(s.getBytes(StandardCharsets.UTF_8));
 	}
 
 }
